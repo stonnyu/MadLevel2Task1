4c8c68546b4bb1c065e973b0b7db0c62c483edb9
Tonny <mrtonnysu@gmail.com>
2020-09-14 00:30:58 +0200

    Wired the adapter to the RecyclerView in the MainActivity

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,14 +2,35 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
+
+private val places = arrayListOf<Place>()
+private val placeAdapter = PlaceAdapter(places)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+
+    fun initViews() {
+        //TODO In initViews set the layout manager (StaggeredGridLayoutManager) and adapter of the RecyclerView.
+        binding.rvPlaces.layoutManager =
+            LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvPlaces.adapter =
+            placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],        Place.PLACE_RES_DRAWABLE_IDS[i]))
+
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
